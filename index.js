ae7a1d97d295eff38b2e7c1d696be500614abed5
Jason Cox <jasonwilburcox@gmail.com>
2017-10-24 00:40:04 +0000

    API making query

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express"),
-    http    = require("http");
+    http    = require("http"),
+    cypher = require('cypher-stream')('bolt://34.228.185.57/', 'neo4j', 'Gateway2');;
 
 /**
  * Config
@@ -14,6 +15,31 @@ var assets = ["data", "src"];
 for (var i=0; i<assets.length; i++) app.use(express.static(__dirname + '/' + assets[i]));
 
 console.log("Serving assets from:", __dirname + '/' + assets);
+
+/**
+ * API
+ */
+app.get('/impacts', function(req, res) {
+  var data = [];
+
+  cypher(
+    'MATCH (n:Impacts)\n' +
+    'WHERE n.PeriodStart <= "2117"\n' +
+    'AND n.PossibleImpacts >= "1"\n' +
+    'RETURN n.ObjectName, n.PeriodStart, n.PossibleImpacts\n' +
+    'ORDER BY toInteger(n.PossibleImpacts) DESC'
+  )
+  .on('data', function (result) {
+    data.push(result);
+  })
+  .on('end', function () {
+    res.send(JSON.stringify(data));
+  });
+});
+
+/**
+ * Server
+ */
 app.listen(3000, function() {
   console.log("Server started");
 });
