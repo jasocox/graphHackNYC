8df64a65388a988ba4352b42b3695f6914f9823a
Jason Cox <jasonwilburcox@gmail.com>
2017-10-24 00:54:06 +0000

    Starting to sort impact

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-  $.get('/impacts', {}, function(data) {
-    console.log('Data:', data);
+  $.get('/impacts', {}, function(impacts) {
+    console.log('Data:', impacts);
+
+    JSON.parse(impacts).forEach(function(impact) {
+      console.log(impact.PeriodStart);
+    });
   });
 });
+
+function addDotToID(id) {
+  console.log('ID:', id);
+}
